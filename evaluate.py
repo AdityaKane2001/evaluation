7745f02dd8b3638cb8695fd71f2a11fb7a5239ce
AdityaKane2001 <adityakane1@gmail.com>
2021-03-17 14:45:04 +0530

    new annotations

--- evaluate.py
+++ evaluate.py
@@ -10,50 +10,102 @@ def are_same(dict1,dict2):
             return False
     return True
 
-def intersects(box1,box2):
-    x11,x12 = box1[0],box1[2]
-    x21,x22 = box2[0],box2[2]
-    y11, y12 = box1[1], box1[3]
-    y21, y22 = box2[1], box2[3]
+# def intersects(box1,box2):
+#     x11,x12 = box1[0],box1[2]
+#     x21,x22 = box2[0],box2[2]
+#     y11, y12 = box1[1], box1[3]
+#     y21, y22 = box2[1], box2[3]
+#
+#     if (x21>=x11 and x21<=x12) or (x22>=x11 and x22<=x12):
+#         if (y21>=y11 and y21<=y12) or (y22>=y11 and y22<=y12):
+#             return True
+#     return False
+#
+#
+# def calc_iou(box1, box2):
+#     #[x1,y1,x2,y2]
+#     if intersects(box1,box2):
+#         all_x = sorted([box1[0],box1[2],box2[0],box2[2]])
+#         int_x1,int_x2 = all_x[1],all_x[2]
+#         all_y = sorted([box1[1],box1[3],box2[1],box2[3]])
+#         int_y1,int_y2 = all_y[1],all_y[2]
+#         intersection = abs(int_x1-int_x2)*abs(int_y1-int_y2)
+#         union = abs((box1[0]-box1[2])*(box1[1]-box1[3]))+abs((box2[0]-box2[2])*(box2[1]-box2[3])) - intersection
+#
+#         iou = intersection/union
+#         if iou>=0:
+#             return iou
+#     return 0
 
-    if (x21>=x11 and x21<=x12) or (x22>=x11 and x22<=x12):
-        if (y21>=y11 and y21<=y12) or (y22>=y11 and y22<=y12):
-            return True
-    return False
+def iou(boxA, boxB):
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA ) * max(0, yB - yA )
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] ) * (boxA[3] - boxA[1] )
+    boxBArea = (boxB[2] - boxB[0] ) * (boxB[3] - boxB[1] )
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    return iou
 
-
-def calc_iou(box1, box2):
-    #[x1,y1,x2,y2]
-    if intersects(box1,box2):
-        all_x = sorted([box1[0],box1[2],box2[0],box2[2]])
-        int_x1,int_x2 = all_x[1],all_x[2]
-        all_y = sorted([box1[1],box1[3],box2[1],box2[3]])
-        int_y1,int_y2 = all_y[1],all_y[2]
-        intersection = abs(int_x1-int_x2)*abs(int_y1-int_y2)
-        union = abs((box1[0]-box1[2])*(box1[1]-box1[3]))+abs((box2[0]-box2[2])*(box2[1]-box2[3])) - intersection
-
-        iou = intersection/union
-        if iou>=0:
-            return iou
-    return 0
+def nms(boxes, overlapThresh):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+    # initialize the list of picked indexes
+    pick = []
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    x2 = boxes[:,2]
+    y2 = boxes[:,3]
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+            np.where(overlap > overlapThresh)[0])))
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    return boxes[pick]
 
 def compute_overlap(a,b):
     overlap_matrix = np.zeros((a.shape[0],b.shape[0]))
     for i in range(len(a)):
         for j in range(len(b)):
-            overlap_matrix[i,j] = calc_iou(a[i],b[j])
+            overlap_matrix[i,j] = iou(a[i],b[j])
 
     return overlap_matrix
 
 
-def get_true_positives(matrix):
-    tp = 0
-    #print(matrix)
-    for i in range(matrix.shape[0]):
-        for j in range(matrix.shape[1]):
-            if matrix[i,j]>0.4:
-                tp+=1
-    return tp
 
 
 def recall(gt,predicted,iou_threshold):
@@ -80,6 +132,11 @@ def recall(gt,predicted,iou_threshold):
             gt_annots = np.array(sorted(gt_annots, key=lambda x: x[1]))
             pred_annots = np.array(sorted(pred_annots, key=lambda x: x[1]))
 
+            #gt_annots = non_max_suppression_fast(gt_annots,0.95)
+            print('GT annots for this image:',len(gt_annots))
+            print('Pred annots for this image before NMS:',len(pred_annots))
+            pred_annots = nms(pred_annots,0.3)
+            print('Pred annots for this image after NMS:', len(pred_annots))
             #print(pred_annots.shape)
             #random.shuffle(pred_annots)
             overlaps = compute_overlap(gt_annots,pred_annots) #(gt_len,pred_len)
@@ -88,6 +145,9 @@ def recall(gt,predicted,iou_threshold):
             #    print('GT key: ',gt_key)
             #    print('predicted key: ',i)
             #print(overlaps.shape)
+            #print('Overlaps: ',overlaps)
+            # print('Get overlaps: ',get_overlap(gt_annots,pred_annots))
+
 
             if overlaps.all()==None:
 
@@ -105,8 +165,10 @@ def recall(gt,predicted,iou_threshold):
 
             else:
                 num_annotations += len(gt_annots)
+                true_positives += len(np.where(overlaps>iou_threshold)[0])
+                print()
 
-                true_positives += get_true_positives(overlaps)
+
         else:
             pass
 
@@ -118,9 +180,9 @@ def recall(gt,predicted,iou_threshold):
 
 
 
-ret = RetinaConverter('valid_annotations.csv')
+ret = RetinaConverter('valid_annotations (1).csv')
 gt = ret()
-predret = RetinaConverter('result_csv.csv')
+predret = RetinaConverter('result_csv (1).csv')
 predicted = predret()
 #print(are_same(gt,predicted))
 
