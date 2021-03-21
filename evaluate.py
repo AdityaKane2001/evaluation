3a74be26b9f48adf6f7ab9556fa8ce353fe36084
AdityaKane2001 <adityakane1@gmail.com>
2021-03-21 13:27:44 +0530

    Backup

--- evaluate.py
+++ evaluate.py
@@ -6,15 +6,9 @@ ret = RetinaConverter('valid_annotations (1).csv')
 gt = ret()
 predret = RetinaConverter('result_csv (2).csv')
 predicted = predret()
-#print(are_same(gt,predicted))
 
-#box_pruning(gt[0])
+print(evaluate(gt,predicted,0.5,0))
 
-print(recall(gt,predicted,0.5,0))
-
-
-#print(predicted)
-#print(gt)
 
 
 
